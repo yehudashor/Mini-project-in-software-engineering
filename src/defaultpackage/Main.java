688bf0cc06771e54f3ba672b9c4780191e1d4ab8
Israel Cohen <josh50170@outlook.co.il>
2021-03-13 22:51:01 +0200

    initial students' details in main class

--- src/defaultpackage/Main.java
+++ src/defaultpackage/Main.java
@@ -8,7 +8,7 @@ import static primitives.Util.*;
  * @author Dan Zilberstein
  * students: 
  * Yehuda Shor 20761055 yehudashor789@gmail.com
- * Israel Cohen 203250170 josh50170@gmail.com
+ * Israel Cohen 203250170 josh50170@gmail.com 
  */
 public final class Main {
 
