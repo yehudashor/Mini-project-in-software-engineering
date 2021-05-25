32666ca2ca30ca3f945c32ff6c424bd7581dfaf6
yehyda shor <yehudashor789@gmail.com>
2021-05-25 20:29:47 +0300

    end of super sumpling girsa b

--- src/unitests/CameraTest.java
+++ src/unitests/CameraTest.java
@@ -26,32 +26,32 @@ public class CameraTest {
 		// ============ Equivalence Partitions Tests ==============
 		// TC01: 3X3 Corner (0,0)	
 		assertEquals("Bad ray", new Ray(Point3D.ZERO, new Vector(-2, -2, 10)),
-		camera.setViewPlaneSize(6, 6).constructRayThroughPixel(3, 3, 0, 0));
+		camera.setViewPlaneSize(6, 6).constructRaysThroughPixel(3, 3, 0, 0, 0).get(0));
 
 		// TC02: 4X4 Corner (0,0)
 		assertEquals("Bad ray", new Ray(Point3D.ZERO, new Vector(-3, -3, 10)),
-				camera.setViewPlaneSize(8, 8).constructRayThroughPixel(4, 4, 0, 0));
+				camera.setViewPlaneSize(8, 8).constructRaysThroughPixel(4, 4, 0, 0,0).get(0));
 
 		// TC03: 4X4 Side (0,1)
 		assertEquals("Bad ray", new Ray(Point3D.ZERO, new Vector(-1, -3, 10)),
-				camera.setViewPlaneSize(8, 8).constructRayThroughPixel(4, 4, 1, 0));
+				camera.setViewPlaneSize(8, 8).constructRaysThroughPixel(4, 4, 1, 0,0).get(0));
 
 		// TC04: 4X4 Inside (1,1)
 		assertEquals("Bad ray", new Ray(Point3D.ZERO, new Vector(-1, -1, 10)),
-				camera.setViewPlaneSize(8, 8).constructRayThroughPixel(4, 4, 1, 1));
+				camera.setViewPlaneSize(8, 8).constructRaysThroughPixel(4, 4, 1, 1,0).get(0));
 
 		// =============== Boundary Values Tests ==================
 		// TC11: 3X3 Center (1,1)
 		assertEquals("Bad ray", new Ray(Point3D.ZERO, new Vector(0, 0, 10)),
-				camera.setViewPlaneSize(6, 6).constructRayThroughPixel(3, 3, 1, 1));
+				camera.setViewPlaneSize(6, 6).constructRaysThroughPixel(3, 3, 1, 1,0).get(0));
 
 		// TC12: 3X3 Center of Upper Side (0,1)
 		assertEquals("Bad ray", new Ray(Point3D.ZERO, new Vector(0, -2, 10)),
-				camera.setViewPlaneSize(6, 6).constructRayThroughPixel(3, 3, 1, 0));
+				camera.setViewPlaneSize(6, 6).constructRaysThroughPixel(3, 3, 1, 0,0).get(0));
 
 		// TC13: 3X3 Center of Left Side (1,0)
 		assertEquals("Bad ray", new Ray(Point3D.ZERO, new Vector(-2, 0, 10)),
-				camera.setViewPlaneSize(6, 6).constructRayThroughPixel(3, 3, 0, 1));
+				camera.setViewPlaneSize(6, 6).constructRaysThroughPixel(3, 3, 0, 1,0).get(0));
 
 	}
 
